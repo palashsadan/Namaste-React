c5c7ad66fe7b0bd3fc353c7ffdfdab3e0b09d624
Palash Sadan <135999607+palashsadan@users.noreply.github.com>
2024-02-06 14:15:11 +0530

    00

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const parent = React.createElement("div",{id: "parent"},[
                 React.createElement("h2", {}, "I am an h2 tag"),
             ])
     ]);
-console.log("parent");
+console.log("parent"); // commeney
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
